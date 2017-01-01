1ff3037bbe80a8821f3302572d1371836ed1609b
Saso Sabotin <sasos90@gmail.com>
2017-01-01 10:54:59 +0100

    First animations on miss + set the hit color from game page

--- src/components/box/box.ts
+++ src/components/box/box.ts
@@ -2,11 +2,13 @@ import {Component, Input} from '@angular/core';
 
 @Component({
     selector: 'box',
-    template: `<div class="box" [ngStyle]="{'background': color}"></div>`
+    template: `<div class="box" [ngStyle]="{'background': color}" (click)="hit()" [ngClass]="{missed: missed}"></div>`
 })
 export class BoxComponent {
 
     @Input("color") color: string = "";
+    @Input("target") target: string = "";
+    public missed: boolean = false;
     text: string;
 
     constructor() {
@@ -16,5 +18,20 @@ export class BoxComponent {
 
     ngOnInit() {
 
+    }
+
+    public hit() {
+        console.debug(this.color);
+        if (this.color === this.target) {
+            console.debug("HIT!");
+            // HIT SUCCESSED
+        } else if (this.missed === true) {
+            console.debug("2. HIT!");
+            // for sure the second hit, which is right!
+        } else if (this.color !== this.target) {
+            console.debug("1. miss");
+            // miss the target
+            this.missed = true;
+        }
     }
 }
--- src/pages/game/game.ts
+++ src/pages/game/game.ts
@@ -3,13 +3,14 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
 @Component({
-  selector: 'game',
-  templateUrl: 'game.html'
+    selector: 'game',
+    templateUrl: 'game.html'
 })
 export class Game {
 
-  constructor(public navCtrl: NavController) {
+    public goalColor: string = "blue";
 
-  }
+    constructor(public navCtrl: NavController) {
 
+    }
 }
