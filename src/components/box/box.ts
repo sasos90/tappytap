a766c1b2808a861c5885b563dadb3744144f5f76
Saso Sabotin <saso.sabotin@scoopandspoon.com>
2017-01-11 07:51:25 +0100

    Naming changed - therefore the logic of checking the HIT is also changed

--- src/components/box/box.ts
+++ src/components/box/box.ts
@@ -2,13 +2,14 @@ import {Component, Input} from '@angular/core';
 
 @Component({
     selector: 'box',
-    template: `<div class="box" [ngStyle]="{'background': color}" (click)="hit()" [ngClass]="{missed: missed}"></div>`
+    template: `<div class="box" [ngStyle]="{'background': color}" (click)="tap()" [ngClass]="{hit: hit}"></div>`
 })
 export class BoxComponent {
 
     @Input("color") color: string = "";
     @Input("target") target: string = "";
-    public missed: boolean = false;
+    public hit: boolean = false;
+    private missed: boolean = false;
     text: string;
 
     constructor() {
@@ -20,18 +21,23 @@ export class BoxComponent {
 
     }
 
-    public hit() {
+    public tap() {
         console.debug(this.color);
         if (this.color === this.target) {
             console.debug("HIT!");
             // HIT SUCCESSED
-        } else if (this.missed === true) {
+            this.hit = true;
+        } else if (this.color !== this.target) {
+            if (!this.missed) {
+                console.debug("1. miss");
+                this.missed = true;
+            } else {
+                console.debug("2. HIT!");
+            }
+
+        } else if (this.hit === false) {
             console.debug("2. HIT!");
             // for sure the second hit, which is right!
-        } else if (this.color !== this.target) {
-            console.debug("1. miss");
-            // miss the target
-            this.missed = true;
         }
     }
 }
