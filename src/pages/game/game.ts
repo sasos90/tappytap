7a84ce71506017e9751c759d036f61770722c83a
Saso Sabotin <sasos90@gmail.com>
2017-01-13 22:45:43 +0100

    Time countdown implemented - just engine

--- src/pages/game/game.ts
+++ src/pages/game/game.ts
@@ -9,8 +9,23 @@ import {BoxModel} from "../../models/BoxModel";
 })
 export class Game {
 
+    // style elements
     private gameWrapper: HTMLElement;
     private gameElement: any;
+
+    // objects
+    /**
+     * Reference ID for requestAnimationFrame javascript function, so we can cancel it later
+     */
+    private rafId: any;
+    /**
+     * Miliseconds when last frame was executed.
+     */
+    private lastFrame: number = null;
+    /**
+     * 5 seconds countdown hardcoded for now.
+     */
+    private countdownTimer: number = 5000;
 
     // TODO: Create MODEL!
     public targetColor: BoxModel = new BoxModel("blue");
@@ -38,6 +53,7 @@ export class Game {
 
         setTimeout(() => {
             this.setLayoutPosition();
+            this.startGame();
         }, 200);
         window.onresize = (event) => {
             this.setLayoutPosition();
@@ -57,5 +73,39 @@ export class Game {
 
         // show the game layout
         this.gameWrapper.classList.remove("invisible");
+    }
+
+    public startGame() {
+        // GAME
+        console.debug("GAME STARTED!");
+        this.rafId = window.requestAnimationFrame((now) => this.rafCallback(now));
+    }
+
+    /**
+     * Each game frame
+     * @param now
+     */
+    private rafCallback(now: number) {
+
+        if (!this.lastFrame) {
+            this.lastFrame = now;
+        }
+        let progress: number = now - this.lastFrame;
+
+        this.rafId = window.requestAnimationFrame((now) => this.rafCallback(now));
+        this.step(progress);
+    }
+
+    /**
+     * Logic of the game with animations.
+     * After 5 seconds the game is done.
+     *
+     * @param progress Miliseconds for actual progress
+     */
+    private step(progress: number) {
+        if (progress > this.countdownTimer) {
+            console.debug("GAME FINISHED!");
+            window.cancelAnimationFrame(this.rafId);
+        }
     }
 }
