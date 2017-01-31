b0c0be5b1bd8ce3dbf6d7e1fcaa7d89a2269462e
Saso Sabotin <sasos90@gmail.com>
2017-01-31 20:27:01 +0100

    Ready set go with animation

--- src/components/ReadySetGo/ReadySetGoComponent.ts
+++ src/components/ReadySetGo/ReadySetGoComponent.ts
@@ -3,27 +3,39 @@ import {BoxModel} from "../../models/BoxModel";
 
 @Component({
     selector: 'ready-set-go',
-    template: `<div class="ready-set-go-wrapper">{{ text }}</div>`
+    template: `
+        <div class="ready-set-go-wrapper">
+            <div class="relative-wrapper">
+                <div class="step one" [ngClass]="{animation: readySetGo >= 0}">{{ 'Ready' }}</div>
+                <div class="step two" [ngClass]="{animation: readySetGo >= 1}">{{ 'Set' }}</div>
+                <div class="step-go" [ngClass]="{animation: readySetGo >= 2}">{{ 'GO!' }}</div>
+            </div>
+        </div>`
 })
 export class ReadySetGoComponent {
 
     @Input() targetBox: BoxModel;
     @Output() countdownFinished = new EventEmitter();
 
-    public text: string;
+    public readySetGo: number = -1;
 
     constructor() {}
 
     ngOnInit() {
-        this.text = "Ready!";
         setTimeout(() => {
-            this.text = "Set!";
+            // initial timeout
+            this.readySetGo++;
             setTimeout(() => {
-                this.text = "GO!";
+                this.readySetGo++;
+                // debugger;
                 setTimeout(() => {
-                    this.countdownFinished.emit();
-                }, 300);
+                    this.readySetGo++;
+                    // debugger;
+                    setTimeout(() => {
+                        this.countdownFinished.emit();
+                    }, 300);
+                }, 1000);
             }, 1000);
-        }, 1000);
+        }, 500);
     }
 }
