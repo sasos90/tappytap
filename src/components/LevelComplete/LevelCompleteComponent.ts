c464744d3fcec038072db9b3d54b7a72a59fbeb7
Saso Sabotin <saso.sabotin@scoopandspoon.com>
2017-03-08 12:12:39 +0100

    Show level completed box after 1 seconds + some styling added to it

--- src/components/LevelComplete/LevelCompleteComponent.ts
+++ src/components/LevelComplete/LevelCompleteComponent.ts
@@ -3,17 +3,25 @@ import {Component, Output, EventEmitter} from '@angular/core';
 @Component({
     selector: 'level-complete',
     template: `
-        <div class="level-complete-wrapper">
+        <div class="level-complete-wrapper material-shadow" *ngIf="shown">
             <div class="message">{{ 'LEVEL COMPLETED' }}</div>
-            <button ion-button (click)="nextLevel()">Next level</button>
+            <div class="navigation-wrapper">
+                <button class="btn-next-level" ion-button (click)="nextLevel()">Next level</button>
+            </div>
         </div>
         <div class="overlay-background"></div>`
 })
 export class LevelCompleteComponent {
 
     @Output("nextLevel") nextLevelEvent = new EventEmitter();
+    public shown: boolean = false;
 
-    constructor() {}
+    constructor() {
+        setTimeout(() => {
+            // show the result after 1 second
+            this.shown = true;
+        }, 1000);
+    }
 
     public nextLevel() {
         this.nextLevelEvent.emit();
