04fee6b3fd43b2c8895641482dc4cd7995362166
Saso Sabotin <saso.sabotin@scoopandspoon.com>
2017-10-04 11:47:24 +0200

    E2E tests prepared

---
+++ e2e/app.e2e-spec.ts
@@ -0,0 +1,11 @@
+import {} from 'jasmine';
+
+describe('Try to play the game', () => {
+
+    beforeEach(() => {
+        console.log('BEFORE EACH');
+    });
+
+    it('should do', () => {
+    });
+});
